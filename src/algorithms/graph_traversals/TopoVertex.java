fc8e091808b3c90b7c44f313e967c039857685d4
Joe Bauer <joseph.bauer_ext@michelin.com>
2019-06-30 20:09:20 -0700

    Minor updates to topological sort problem

--- src/algorithms/graph_traversals/TopoVertex.java
+++ src/algorithms/graph_traversals/TopoVertex.java
@@ -7,7 +7,6 @@ public class TopoVertex {
     public String label;
     public List<TopoVertex> edges = new ArrayList<>();
     public int maxDistance = 0;
-    public boolean visited = false;
 
     public TopoVertex(String label) {
         this.label = label;
