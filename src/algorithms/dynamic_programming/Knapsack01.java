c6009ad1351651e1515da4d5cf41ae772f5a45ed
jmbauer <r81moss@gmail.com>
2019-06-30 17:28:21 -0700

    Update Knapsack01.java

--- src/algorithms/dynamic_programming/Knapsack01.java
+++ src/algorithms/dynamic_programming/Knapsack01.java
@@ -1,28 +1,32 @@
 package algorithms.dynamic_programming;
 
 public class Knapsack01 {
-    public static void printKnapSack(int weights[], int values[], int capacity) {
-        int knapsackValues[][] = new int[values.length + 1][capacity + 1];
+    public static void printKnapSack(int[][] items, int capacity) {
+        int knapsackValues[][] = new int[items.length + 1][capacity + 1];
 
         // start at 1 since 0 weight and 0 items would be 0
-        for (int i = 1; i <= values.length; i++) {
-            for (int w = 1; w <= capacity; w++) {
-                if (weights[i - 1] <= w) {
-                    knapsackValues[i][w] = Math.max(values[i - 1] +
-                            knapsackValues[i - 1][w - weights[i - 1]], knapsackValues[i - 1][w]);
+        for (int i = 1; i < items.length + 1; i++) {
+            int currentWeight = items[i-1][1];
+            int currentValue = items[i-1][0];
+            for (int w = 1; w < capacity + 1; w++) {
+                if (currentWeight <= w) {
+                    knapsackValues[i][w] = Math.max(currentValue +
+                            knapsackValues[i - 1][w - currentWeight], knapsackValues[i - 1][w]);
                 } else {
                     knapsackValues[i][w] = knapsackValues[i - 1][w];
                 }
             }
         }
 
-        int res = knapsackValues[values.length][capacity];
+        int res = knapsackValues[items.length][capacity];
         System.out.println(res);
-        for (int i = values.length, w = capacity; i > 0 && res > 0; i--) {
+        for (int i = items.length, w = capacity; i > 0 && res > 0; i--) {
             if (res != knapsackValues[i - 1][w]) {
-                System.out.print(weights[i - 1] + " ");
-                res -= values[i - 1];
-                w -= weights[i - 1];
+                int currentWeight = items[i-1][1];
+                int currentValue = items[i-1][0];
+                System.out.print(currentWeight + " ");
+                res -= currentValue;
+                w -= currentWeight;
             }
         }
     }
